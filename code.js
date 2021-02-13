5cb5c627970acd02e543e36715851407ac30dee2
Elizabeth Rogers <elle.eels@gmail.com>
2021-02-12 23:01:41 -0500

    new array logic

--- code.js
+++ code.js
@@ -1,5 +1,23 @@
 var inputValue
+var inputArray
 $(document).on("click", "#submitButton", function(){
-    inputValue = $('#inputBox').val()
+    inputValue = $('#inputBox').val();
+    $('#output').html('')
     console.log(inputValue)
-})
+    //document.getElementById("output").innerHTML = inputValue
+    $('#inputBox').val('')
+    inputArray = inputValue.split(" ")
+    output = inputArray
+    var i;
+    for (i = 0; i < output.length; i++) {
+        if (output[i] == "the") {
+            $("#output").append("<span id='the'>" + output[i] + "</span> ")
+        }
+        else if (output[i] == "The") {
+            $("#output").append("<span id='the'>" + output[i] + "</span> ")
+        }
+        else
+       $("#output").append("<span>" + output[i] + "</span> ")
+      }
+   // document.getElementById("output").innerHTML = output
+});
